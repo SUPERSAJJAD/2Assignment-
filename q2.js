acd7bffffe9bbe3e84a5b0619840e2657fee2c60
Sajjad Lashari <khansahab0289@gmail.com>
2024-03-10 21:56:57 +0500

    2nd Assignment #45Assignments # GovernerSindhIt

---
+++ q2.js
@@ -0,0 +1,2 @@
+var personName = "Sajjad";
+console.log("Hello ".concat(personName, " Would you like to learn some python today"));
---
+++ q2.ts
@@ -0,0 +1,2 @@
+let personName: string="Sajjad"
+console.log(`Hello ${personName} Would you like to learn some python today`)
